41cb574c1f50cbef989a8d5ca5620ce2a2788f0c
zhangshen <zhangshen@shanghaitech.edu.cn>
2025-12-16 12:39:01 +0800

    update grouped output ports

--- va_gen.py
+++ va_gen.py
@@ -116,11 +116,29 @@ class Signal:
         
         return template.format(arg_1, arg_2, arg_3)
 
+    def get_signal_grouped_output(self, bus_name, bus_offset ):
+        template = '''
+        for(i=0; i<{}; i=i+1) begin
+            V( {}[{}+i] ) <+ transition( V(VDD,GND)*(({}&(1<<i))>>i), 0, 0 ) ;
+        end
+        '''
+        arg_1 = self.width
+        arg_2 = bus_name
+        arg_3 = bus_offset
+        arg_4 = self.signal_name
+        return template.format(arg_1, arg_2, arg_3, arg_4)
+
+    def get_signal_output_loop(self, bus_name=None, bus_offset=0):
+        if bus_name != None:
+            return self.get_signal_grouped_output(bus_name, bus_offset)
+        else:
+            return self.get_signal_output()
 
 class MultiSignal:
-    def __init__(self, args = []):
+    def __init__(self, args = [], port_groups=None):
 
         self.signals = self.get_signals(args=args)
+        self.port_groups = port_groups or {}
 
         self.port_list              = self.get_port_list()
         self.input_declaration      = ''
@@ -132,7 +150,7 @@ class MultiSignal:
         self.count_initial          = self.get_count_initial()
         self.signal_generate        = self.get_signal_generate()
         self.count_generate         = self.get_count_generate()
-        self.signal_output          = self.get_signal_output()
+        self.signal_output          = self.get_signal_output(port_groups=port_groups)
 
     def get_signals(self, args):
         thelist = []
@@ -144,20 +162,38 @@ class MultiSignal:
             thelist.append(the_signal)
         return thelist
 
-
     def get_port_list(self):
-        result = ''
-        for i in self.signals[:-1]:
-            result = result + ' {},'.format(i.name)
-        result = result + ' {}'.format(self.signals[-1].name)
-        return result
+        ports = []
+        # 1. 聚合端口
+        for group_name in self.port_groups:
+            ports.append(group_name)
+        # 2. 非聚合的原始信号
+        grouped = set(sum(self.port_groups.values(), []))
+        for sig in self.signals:
+            if sig.name not in grouped:
+                ports.append(sig.name)
+        return ', '.join(ports)
 
     def get_output_declaration(self):
         result = ''
-        for i in self.signals:
-            result = result + i.output_declaration +'\n'
+
+        grouped = set(sum(self.port_groups.values(), []))
+
+        # 1. group ports
+        for name, members in self.port_groups.items():
+            width = sum(
+                sig.width for sig in self.signals if sig.name in members
+            )
+            result += f'    output [{width-1}:0] {name};\n'
+            result += f'    electrical [{width-1}:0] {name};\n\n'
+        # 2. internal member signals (NOT output!)
+        for sig in self.signals:
+            if sig.name in grouped:
+                result += f'    electrical {sig.width_declaration} {sig.name};\n'
+            else:
+                result += sig.output_declaration + '\n'
         return result
-    
+
     def get_signal_declaration(self):
         result = ''
         for i in self.signals:
@@ -200,12 +236,24 @@ class MultiSignal:
             result = result + i.count_generate +'\n'
         return result  
 
-    def get_signal_output(self):
+    def get_signal_output(self, port_groups=None):
         result = ''
-        for i in self.signals:
-            result = result + i.signal_output +'\n'
-        return result  
+        signal_dict = {sig.name: sig for sig in self.signals}
+        unseen_set = set( list(signal_dict.keys()) )
+        if port_groups:
+            for bus_name, members in port_groups.items():
+                bit_offset = 0
+                for member_name in reversed(members):
+                    unseen_set.remove(member_name)
+                    sig = signal_dict[member_name]
+                    result += sig.get_signal_output_loop(bus_name, bit_offset)
+                    bit_offset += sig.width
 
+        for sig_name in unseen_set:
+            sig = signal_dict[sig_name]
+            result += sig.get_signal_output_loop()
+
+        return result
 
 class VaGenerator:
     def __init__(self, file_path):
@@ -223,12 +271,12 @@ class VaGenerator:
                                 '$TEMPLATE_COUNT_INITIAL',
                                 '$TEMPLATE_SIGNAL_GENERATE',
                                 '$TEMPLATE_COUNT_GENERATE',
-                                '$TEMPLATE_SIGNAL_OUTPUT'
+                                '$TEMPLATE_SIGNAL_OUTPUT',
                                 ]
-    def generate_veriloga(self, module_name, comment, args, save_path):
+    def generate_veriloga(self, module_name, comment, args, save_path, port_groups=None):
         self.module_name = module_name
         self.comment = comment
-        self.generate_signals(args=args)
+        self.generate_signals(args=args, port_groups=port_groups)
         self.fill_template()
         self.save_file(file_path=save_path)
 
@@ -237,8 +285,8 @@ class VaGenerator:
             str_template = f.read()
         return str_template 
 
-    def generate_signals(self, args):
-        thesignals = MultiSignal(args=args)
+    def generate_signals(self, args, port_groups):
+        thesignals = MultiSignal(args=args, port_groups=port_groups)
         self.signals = [    self.comment,
                             self.module_name,
                             thesignals.port_list,
@@ -251,7 +299,8 @@ class VaGenerator:
                             thesignals.count_initial ,
                             thesignals.signal_generate ,
                             thesignals.count_generate ,
-                            thesignals.signal_output ]
+                            thesignals.signal_output ,
+                            ]
 
     def fill_template(self):
         content = self.template
